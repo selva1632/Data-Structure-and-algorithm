fddcda1b909ee57005c21b0f7093dc66c6cdad15
selva1632 <selvabharathy1632@gmail.com>
2025-01-12 17:49:53 +0530

    add 200. Number of Islands

---
+++ Graph/200. Number of Islands/200. Number of Islands.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    void dfs(int x, int y, vector<vector<char>>& grid, int n, int m) {
+        if (x < 0 || y < 0 || x >= n || y >= m || grid[x][y] == '0') 
+            return;
+        
+        grid[x][y] = '0';
+        dfs(x + 1, y, grid, n, m);
+        dfs(x, y + 1, grid, n, m);
+        dfs(x - 1, y, grid, n, m);
+        dfs(x, y - 1, grid, n, m);
+    }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(i, j, grid, n, m);
+                }
+            }
+        }
+
+        return count;
+    }
+};
