2bd69cc58cc9c4188acafd61dad503e158a0b3a9
selva1632 <selvabharathy1632@gmail.com>
2025-01-19 12:49:25 +0530

    Utils-2

--- BinarySearch/Utils/utils.cpp
+++ BinarySearch/Utils/utils.cpp
@@ -51,3 +51,35 @@ int lowerBound(vector<int>& arr, int x) {
     } 
     return ans;
 }
+
+int findFloor(vector<int>& arr, int n, int x) {
+	int l = 0, r = n - 1, ans = -1;
+
+	while (l <= r) {
+		int mid = l + (r - l) / 2;
+		
+		if (arr[mid] <= x) {
+			ans = arr[mid];
+			l = mid + 1; 
+		} else {
+			r = mid - 1;
+		}
+	}
+	return ans;
+}
+int findCeil(vector<int>& arr, int n, int x) {
+	int l = 0, r = n - 1, ans = -1;
+
+	while (l <= r) {
+		int mid = l + (r - l) / 2;
+
+		if (arr[mid] >= x) {
+			ans = arr[mid];
+			r = mid - 1;
+		} else {
+			l = mid + 1;
+		}
+	}
+
+	return ans;
+}
