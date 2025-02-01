f0b5fb7b25e1a313993c5c41fbaa74fb67beb716
selva1632 <selvabharathy1632@gmail.com>
2025-02-01 13:37:25 +0530

    416. Partition Equal Subset Sum

---
+++ DP/416. Partition Equal Subset Sum/416. Partition Equal Subset Sum.cpp
@@ -0,0 +1,42 @@
+// problem link: https://leetcode.com/problems/partition-array-into-two-arrays-to-minimize-sum-difference/
+
+#include<iostream>
+#include<vector>
+#include<numeric>
+using namespace std;
+
+class Solution {
+private:
+    bool f(int ind, int target, vector<int>& nums, vector<vector<int>> &dp) {
+        if (ind == 0) {
+            if (target == nums[0]) {
+                return true;
+            }
+            return false;
+        }
+
+        if (dp[ind][target] != -1) return dp[ind][target];
+
+        bool notpick = f(ind - 1, target, nums, dp);
+        bool pick = false;
+        if (nums[ind] <= target) {
+            pick = f(ind - 1, target - nums[ind], nums, dp);
+        }
+        return dp[ind][target] = pick || notpick;
+    }
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum % 2 == 1) {
+            return false;
+        }
+        vector<vector<int>> dp(n, vector<int>(sum / 2 + 1, -1));
+        return f(n - 1, sum / 2, nums, dp);
+    }
+};
+
+/*
+    time complexity - O(n * sum)
+    space complexity -  O(n * sum) + O(N)
+*/
