cccd495cbf432933b9ca8cac59aa82c2dcfc3417
selva1632 <selvabharathy1632@gmail.com>
2025-02-01 17:56:30 +0530

    322. Coin Change

--- DP/322. Coin Change/322. Coin Change.cpp
+++ DP/322. Coin Change/322. Coin Change.cpp
@@ -1,4 +1,4 @@
-// problem link: https://leetcode.com/problems/coin-change-ii/
+// problem link: https://leetcode.com/problems/coin-change/
 
 #include<iostream>
 #include<vector>
@@ -8,31 +8,29 @@ using namespace std;
 class Solution {
 private:
     int f(int ind, int target, vector<int>& coins, vector<vector<int>>& dp) {
-        if (target == 0) {
-            return 1;
-        }
         if (ind == 0) {
-            if (target % coins[ind] == 0) return 1;
-            return 0;
+            if (target % coins[0] == 0) return target / coins[0];
+            return 1e9;
         }
 
         if (dp[ind][target] != -1) return dp[ind][target];
 
         int notpick = f(ind - 1, target, coins, dp);
-        int pick = 0;
+        int pick = 1e9;
         if (coins[ind] <= target) {
-            pick = f(ind, target - coins[ind], coins, dp);
+            pick = 1 + f(ind, target - coins[ind], coins, dp);
         }
 
-        return dp[ind][target] = pick + notpick;
+        return dp[ind][target] = min(pick, notpick);
     }
 public:
-    int change(int amount, vector<int>& coins) {
+    int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, - 1));
-        return f(n-1, amount, coins, dp);
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        int ans = f(n-1, amount, coins, dp);
+        return ans >= 1e9 ? - 1: ans;
     }
-};
+};  
 
 /*
     Time Complexity: O(n × amount)
