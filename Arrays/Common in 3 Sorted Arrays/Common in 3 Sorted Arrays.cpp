98e5d95ac169ec4f22ea7e92cc16b668c69429f0
selva1632 <selvabharathy1632@gmail.com>
2025-02-20 22:09:56 +0530

    Kth element in Matrix

--- Arrays/Common in 3 Sorted Arrays/Common in 3 Sorted Arrays.cpp
+++ Arrays/Common in 3 Sorted Arrays/Common in 3 Sorted Arrays.cpp
@@ -31,3 +31,29 @@ class Solution {
         return res;
     }
 };
+
+int kthSmallest(int mat[MAX][MAX], int n, int k)
+{
+  //Your code here
+    priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+  
+  for (int i = 0; i < n; i++) {
+      pq.push({mat[i][0], i, 0});
+  }
+  
+  int ans;
+  while (k--) {
+      int val = pq.top()[0];
+      int row = pq.top()[1];
+      int col = pq.top()[2];
+      pq.pop();
+      
+      ans = val;
+      
+      if (col + 1 < n) {
+          pq.push({mat[row][col + 1], row, col + 1});
+      }
+  }
+  
+  return ans;
+}
